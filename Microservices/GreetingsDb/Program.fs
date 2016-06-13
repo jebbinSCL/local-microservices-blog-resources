800f3af2e06535eb57d243a12297e413e1f4c146
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-06-13 14:08:00 +0100

    Add Greetings Db project

---
+++ Microservices/GreetingsDb/Program.fs
@@ -0,0 +1,8 @@
+﻿open Suave
+open Suave.Http
+
+[<EntryPoint>]
+let main _ = 
+    let config = { defaultConfig with bindings = [ HttpBinding.mkSimple HTTP "0.0.0.0" 8080 ] }
+    startWebServer config (Successful.OK "Hello World from Database!")
+    0
