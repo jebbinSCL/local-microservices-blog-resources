f28a3a5f0c5233e78f076ea4eae6789928ea7b42
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-06-14 10:56:44 +0100

    Move greetingsDb network config to seperate file

--- Microservices/GreetingsDb/Program.fs
+++ Microservices/GreetingsDb/Program.fs
@@ -1,8 +1,12 @@
 ﻿open Suave
 open Suave.Http
+open Config
+
+let serviceIp = getConfigValue "ServiceIp"
+let servicePort = getIntConfig "ServicePort"
 
 [<EntryPoint>]
 let main _ = 
-    let config = { defaultConfig with bindings = [ HttpBinding.mkSimple HTTP "0.0.0.0" 8080 ] }
+    let config = { defaultConfig with bindings = [ HttpBinding.mkSimple HTTP serviceIp servicePort ] }
     startWebServer config (Successful.OK "Hello World from Database!\n")
     0
