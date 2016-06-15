ede95c2467ed80f28e7be606ad06ed9aadc46c21
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-06-15 16:00:34 +0100

    Reorder script execution

--- Microservices/buildServices.sh
+++ Microservices/buildServices.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -eu
 echo "Building Services"
+sh -eu ./setupBuildTools.sh
 mono packages/FAKE/tools/FAKE.exe build.fsx
--- Microservices/oneUp.sh
+++ Microservices/oneUp.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -eu
 
-sh -eu ./setupBuildTools.sh
 sh -eu ./buildServices.sh
 sh -eu ./buildImages.sh
 sh -eu ./up.sh
